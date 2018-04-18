84388e760fb78170bf2ecd37be48b9e1c944e12a
asdf1234Damian <asdf1234damian@gmail.com>
2018-04-17 21:45:26 -0500

    Practica3 en una sola GAL

--- Practica3.vhdl
+++ Practica3.vhdl
@@ -8,15 +8,19 @@ entity SumadorRestador is
     S:out std_logic_vector(3 downto 0);
     Cf,OVERFLOW:out std_logic
   );
+  attribute pin_numbers of SumadorRestador: entity is
+  "A(0):2 A(1):3 A(2):4 A(3):5 B(0):6 B(1):7 B(2):8 B(3):9 Ci:10 S(0):19 S(1):20 S(2):21 S(3):22 OVERFLOW:23";
 end entity;
 
 architecture archSumadorRestador  of  SumadorRestador is
 
   signal C: std_logic_vector(4 downto 1);
-
+  attribute synthesis_off of c: signal is true;
+  signal SB: std_logic_vector(3 downto 0);
+  attribute synthesis_off of SB: signal is true;
 begin
-SB : for i in 0 to 3 generate
-  B(i)<= Ci xor B(i);
+BtoSB : for i in 0 to 3 generate
+  SB(i)<= Ci xor B(i);
 end generate;
 S(0)<=A(0) xor B(0) xor Ci;
 C(1)<=(A(0) and (B(0) or Ci)) or (Ci and B(0));
