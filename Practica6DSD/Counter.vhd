8d8c73e703df7ddbc0d1a8125bfa6822a1ff660a
Damián Meza Madrid <asdf1234damian@gmail.com>
2018-10-08 18:40:28 -0500

    Correcciones Practica 6

--- Practica6DSD/Counter.vhd
+++ Practica6DSD/Counter.vhd
@@ -19,15 +19,57 @@ architecture arch_counter of Counter is
 	attribute NOM_FREQ: string ;
 	attribute NOM_FREQ of OSCinst0 : label is "133.00";
 	signal d: std_logic_vector(27 downto 0);
+	signal clkA, clkB, clkC, clkD: std_logic;
 begin 
 	OSCInst0 : OSCH GENERIC  MAP("133.00") PORT MAP('0', clk);
-	counter:process (clk) is 	
+	p_ClkA:process (clk) is 	
+	variable count: integer range 0 to 33250000;
 	begin 
 		if(clk'event and clk='1') then 
-			if(d="11111111111111111111111111111") then 
-				d<="0000000000000000000000000000";
+			if(d<33250000) then 
+				count:=count+1;
 			else
-				d<=d+1;
+				count:=0;
+				clkA<= not clkA;
+			end if;
+		end if; 
+	end process; 
+	
+	p_ClkB:process (clk) is 	
+	variable count: integer range 0 to 66500000;
+	begin 
+		if(clk'event and clk='1') then 
+			if(d<66500000) then 
+				count:=count+1;
+			else
+				count:=0;
+				clkB<= not clkB;
+			end if;
+		end if; 
+	end process; 
+	
+	p_ClkC:process (clk) is 	
+	variable count: integer range 0 to 133000000;
+	begin 
+		if(clk'event and clk='1') then 
+			if(d<133000000) then 
+				count:=count+1;
+			else
+				count:=0;
+				clkC <= not clkC;
+			end if;
+		end if; 
+	end process; 
+	
+	p_clkD:process (clk) is 	
+	variable count: integer range 0 to 166250000;
+	begin 
+		if(clk'event and clk='1') then 
+			if(d<166250000) then 
+				count:=count+1;
+			else
+				count:=0;
+				clkD <= not clkD;
 			end if;
 		end if; 
 	end process; 
@@ -35,12 +77,12 @@ begin
 	
 	mux:process (clk)is 
 	begin 
-		case (sel) is 
-			when "00" => o <= d(25); --f=53.2=2hz
-			when "01" => o <= d(24);
-			when "10" => o <= d(23);
-			when "11" => o <= d(22); 
-			when others => o <= '0';
+		case (sel) is  
+			when "00" => o <= clkA; 
+			when "01" => o <= clkB;
+			when "10" => o <= clkC;
+			when "11" => o <= clkD; 
+			when others => o <= '0' ;
 		end case;
 	end process;
 end architecture;    
