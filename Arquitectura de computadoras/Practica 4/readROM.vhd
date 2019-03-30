751171eb9a4c33df3dcc86c508f9827d4f28f352
asdf1234Damian <asdf1234damian@gmail.com>
2019-03-30 17:48:32 -0600

    Read rom not tested

--- Arquitectura de computadoras/Practica 4/readROM.vhd
+++ Arquitectura de computadoras/Practica 4/readROM.vhd
@@ -9,7 +9,7 @@ entity readROm is
     ROM_address : out std_logic_vector(7 downto 0),
     ROM_res : out std_logic_vector(7 downto 0),
     ROM_CE: std_logic,
-    ROM_: std_logic;
+    ROM_OE: std_logic;
   );
 end entity;
 
@@ -19,13 +19,34 @@ architecture readRom_Arch of readRom is
   signal counter : integer:=0;
 begin
   case(state) is
+    ROM_address<='ZZZZZZZZ';
+    FPGA_res<='ZZZZZZZZ';
     when 0 =>
-      ROM_address<='ZZZZZZZZ';
       ROM_CE <= 1;
       ROM_CE <= 1;
       counter+;
-      
-    when IDLE =>
+      if counter < 2 then
+        counter:= 0;
+        state:= 1;
+      end if
+    when 1 =>
+      ROM_address<=FPGA_address;
+      ROM_CE <= 0;
+      ROM_OE <= 0;
+      counter+;
+      if counter < 5 then
+        counter:= 0;
+        state:= 2;
+        FPGA_res <= ROM_res;
+      end if
     when others =>
+      FPGA_res <= ROM_res;
+      ROM_CE <= 0;
+      ROM_OE <= 0;
+      counter+;
+      if counter < 1 then
+        counter:= 0;
+        state:= 0;
+      end if
   end case;
 end architecture;
